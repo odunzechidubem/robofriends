1992c0d5d4733492b6f80b9b7410b22a20905acf
odunzechidubem <90936259+odunzechidubem@users.noreply.github.com>
2022-07-25 11:16:11 +0100

    Add files via upload

---
+++ reactjs/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Card from './Card';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.createRoot(
+                                <div>
+                                  <Card />
+                                  <Card />
+                                  <Card />
+                                </div>
+  , document.getElementById('root'));
+  reportWebVitals();
+
+// If you want to start measuring performance in your Card, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+gh repo clone odunzechidubem/reactjs
